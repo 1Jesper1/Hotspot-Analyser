aba7805ca05ad213622f20dc7c8f74ef3dcf099e
Jesper <1jesper1@gmail.com>
2015-09-30 17:28:35 +0200

    Changed SSID to multiline, Created start of open network connect function to check for captive portal

--- app/src/main/java/com/hro/hotspotanalyser/adapters/NetworkListAdapter.java
+++ app/src/main/java/com/hro/hotspotanalyser/adapters/NetworkListAdapter.java
@@ -2,6 +2,10 @@ package com.hro.hotspotanalyser.adapters;
 
 import android.content.Context;
 import android.net.wifi.ScanResult;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +20,10 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class NetworkListAdapter extends ArrayAdapter<ScanResult> {
+
+    WifiManager wifi;
+    SupplicantState supState;
+    Boolean captivePortal;
 
     public NetworkListAdapter(Context context, List<ScanResult> objects) {
         super(context, 0, objects);
@@ -35,6 +43,9 @@ public class NetworkListAdapter extends ArrayAdapter<ScanResult> {
         } else {
             viewHolder = (NetworkItemViewHolder) convertView.getTag();
         }
+
+        boolean captivePortal = hasCaptivePortal(result);
+
 
         viewHolder.ssid.setText(result.SSID);
         viewHolder.bssid.setText(result.BSSID);
@@ -57,6 +68,39 @@ public class NetworkListAdapter extends ArrayAdapter<ScanResult> {
 
         public NetworkItemViewHolder(View itemView) {
             ButterKnife.bind(this, itemView);
+        }
+    }
+
+    //Funtion to check for captive portal
+    public Boolean hasCaptivePortal(ScanResult result){
+        //If network is not open
+        if(getAuthenticationType(result.capabilities) != "OPEN") {
+            return false;
+        }
+        //If network is open
+        else {
+            WifiConfiguration wifiConfiguration = new WifiConfiguration();
+            wifiConfiguration.SSID = result.SSID;
+            wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            wifiConfiguration.BSSID = result.BSSID;
+            wifiConfiguration.hiddenSSID = false;
+
+            wifi = (WifiManager) this.getContext().getSystemService(Context.WIFI_SERVICE);
+            int networkID = wifi.addNetwork(wifiConfiguration);
+            wifi.disconnect();
+            wifi.enableNetwork(networkID, true);
+            wifi.reconnect();
+            //Check for captive portal
+            /*if(CAPTIVE PORTAL}}{
+                WifiInfo wifiInfo = wifi.getConnectionInfo();
+                supState = wifiInfo.getSupplicantState();
+                return true;
+            }
+            //Not captive portal
+            else{
+                return false;
+            }*/
+
         }
     }
 
