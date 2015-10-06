7a439213f12ab01ef39b1fd1cdd37cf04018b4bc
Schamper <mrwafflewaffle@aim.com>
2015-10-06 13:08:40 +0200

    AsyncTask

--- app/src/main/java/com/hro/hotspotanalyser/receivers/WifiReceiver.java
+++ app/src/main/java/com/hro/hotspotanalyser/receivers/WifiReceiver.java
@@ -7,6 +7,7 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.util.Log;
 
@@ -46,54 +47,61 @@ public class WifiReceiver extends BroadcastReceiver {
             NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
 
             if (info != null && info.isConnected()) {
-                analyzeNetwork(context, wifiManager);
+                new AnalyzeNetworkTask().execute(wifiManager);
             }
         }
 
     }
 
-    private void analyzeNetwork(Context context, WifiManager wifiManager) {
-        double safetyMeter = 0.0;
-        WifiConfiguration currentConfig = null;
+    private static final class AnalyzeNetworkTask extends AsyncTask<WifiManager, Void, Void> {
+        @Override
+        protected Void doInBackground(WifiManager... wifiManagers) {
+            WifiManager wifiManager = wifiManagers[0];
 
-        for (WifiConfiguration conf : wifiManager.getConfiguredNetworks()) {
-            if (conf.status == WifiConfiguration.Status.CURRENT) {
-                currentConfig = conf;
+            double safetyMeter = 0.0;
+            WifiConfiguration currentConfig = null;
+
+            for (WifiConfiguration conf : wifiManager.getConfiguredNetworks()) {
+                if (conf.status == WifiConfiguration.Status.CURRENT) {
+                    currentConfig = conf;
+                }
             }
+
+            // Only analyze when there's no protection
+            if (currentConfig != null && currentConfig.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.NONE)) {
+                boolean isCaptivePortal = checkCaptivePortal(wifiManager);
+                Log.d(LOG_TAG, "Is captive portal: " + isCaptivePortal);
+            }
+
+            return null;
         }
 
-        // Only analyze when there's no protection
-        if (currentConfig != null && currentConfig.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.NONE)) {
-            boolean isCaptivePortal = checkCaptivePortal(wifiManager);
-            Log.d(LOG_TAG, "Is captive portal: " + isCaptivePortal);
-        }
-    }
+        private boolean checkCaptivePortal(WifiManager wifiManager) {
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            NetworkInfo.DetailedState detailedState = WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState());
 
-    private boolean checkCaptivePortal(WifiManager wifiManager) {
-        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        NetworkInfo.DetailedState detailedState = WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState());
+            if (detailedState == NetworkInfo.DetailedState.CAPTIVE_PORTAL_CHECK) {
+                return true;
+            } else {
+                HttpURLConnection urlConnection = null;
+                try {
+                    URL url = new URL(WALLED_GARDEN_URL); // "http://clients3.google.com/generate_204"
 
-        if (detailedState == NetworkInfo.DetailedState.CAPTIVE_PORTAL_CHECK) {
-            return true;
-        } else {
-            HttpURLConnection urlConnection = null;
-            try {
-                URL url = new URL(WALLED_GARDEN_URL); // "http://clients3.google.com/generate_204"
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setInstanceFollowRedirects(false);
+                    urlConnection.setConnectTimeout(WALLED_GARDEN_SOCKET_TIMEOUT_MS);
+                    urlConnection.setReadTimeout(WALLED_GARDEN_SOCKET_TIMEOUT_MS);
+                    urlConnection.setUseCaches(false);
+                    urlConnection.getInputStream();
 
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setInstanceFollowRedirects(false);
-                urlConnection.setConnectTimeout(WALLED_GARDEN_SOCKET_TIMEOUT_MS);
-                urlConnection.setReadTimeout(WALLED_GARDEN_SOCKET_TIMEOUT_MS);
-                urlConnection.setUseCaches(false);
-                urlConnection.getInputStream();
-
-                // We got a valid response, but not from the real google
-                return urlConnection.getResponseCode() != 204;
-            } catch (IOException e) {
-                return false;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
+                    // We got a valid response, but not from the real google
+                    return urlConnection.getResponseCode() != 204;
+                } catch (IOException e) {
+                    return false;
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
                 }
             }
         }
