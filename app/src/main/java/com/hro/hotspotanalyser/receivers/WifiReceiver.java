b9b8f1e92226a16b40fcba42e553b66f4829fbb3
Schamper <mrwafflewaffle@aim.com>
2015-10-13 12:40:15 +0200

    Return false if exception for certs

--- app/src/main/java/com/hro/hotspotanalyser/receivers/WifiReceiver.java
+++ app/src/main/java/com/hro/hotspotanalyser/receivers/WifiReceiver.java
@@ -189,6 +189,7 @@ public class WifiReceiver extends BroadcastReceiver {
                     return false;
                 } catch (IOException | CertificateExpiredException | CertificateNotYetValidException e) {
                     e.printStackTrace();
+                    return false;
                 } finally {
                     if (conn != null) {
                         conn.disconnect();
