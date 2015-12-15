91d8192c3be70403bc8def547fe3af209961b8e6
Jesper <1jesper1@gmail.com>
2015-12-15 20:42:16 +0100

    Created Google HTTP and HTTPS unit test for certificates check

--- app/src/test/java/com/hro/hotspotanalyser/ExampleUnitTest.java
+++ app/src/test/java/com/hro/hotspotanalyser/ExampleUnitTest.java
@@ -2,14 +2,59 @@ package com.hro.hotspotanalyser;
 
 import org.junit.Test;
 
+import java.lang.Exception;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    //Test should fail because of HTTP url cast to HTTPS url connection
+    public void certificateCheckHTTP() throws Exception   {
+                try {
+                    assertEquals(X509Certificate[].class, getCertificates("http://google.com/").getClass());
+                    fail("Should have thrown an exception");
+                }
+                catch(Exception e){
+                    assertEquals("sun.net.www.protocol.http.HttpURLConnection cannot be cast to javax.net.ssl.HttpsURLConnection", e.getMessage());
+                }
+    }
+    @Test
+    //Test should pass, can only fail when there is no internet connection or website is offline
+    public void certificateCheckHTTPS() throws Exception {
+                try {
+                    assertEquals(X509Certificate[].class, getCertificates("https://google.com/").getClass());
+                }
+                catch(Exception e){
+                    fail("Should not thrown an exception, maybe there is no internet connection");
+                }
+    }
+    private X509Certificate[] getCertificates(String portalUrl) throws Exception{
+        HttpsURLConnection conn = null;
+            try {
+
+                URL url = new URL(portalUrl);
+                conn = (HttpsURLConnection) url.openConnection();
+                conn.getInputStream();
+
+                //Get server certificates
+                Certificate[] certificates = conn.getServerCertificates();
+                return Arrays.copyOf(certificates, certificates.length, X509Certificate[].class);
+            } catch (Exception e) {
+                throw new Exception(e.getMessage());
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
     }
 }
